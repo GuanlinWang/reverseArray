ad00bcd63c84d31c5046f8302a194c778934cc2c
GuanlinWang <43226744+GuanlinWang@users.noreply.github.com>
2019-02-07 19:50:45 -0800

    Update reverseArray.cpp

--- reverseArray.cpp
+++ reverseArray.cpp
@@ -3,12 +3,27 @@
 using namespace std;
 void reverseArray(int array[],int size)
 {
+  int temp,temp2;
+  int count=1;
+  for (int i =0; i<size/2;i++)
+  {
+       temp=array[i];
+      temp2=array[size-count];
+      array[i]=temp2;
+      array[size-count]=temp;
+      count++;
+  }
+  for (int i =0;i<size;i++)
+  {
+    cout<<array[i]<<" ";
+  }
+  cout<<endl;
 
 }
 int main()
 {
   int array[4]={1,2,3,4};
-
+  reverseArray(array,4);
   return 0;
 
 }
